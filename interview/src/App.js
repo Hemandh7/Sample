73a43a9d8f27311afc8c9ba14b86b1700c3d95ae
Hemandh <hemandh007bond@gmail.com>
2023-06-24 01:02:28 +0530

    interview

---
+++ interview/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Routes, Route, NavLink } from 'react-router-dom';
+// import Home from './Components/Home';
+import Quiz from './Components/Quiz';
+// import Statistics from './Components/Statistics';
+
+const App = () => {
+  return (
+    <div>
+      <Quiz/>
+    </div>
+  );
+};
+
+export default App;
---
+++ interview/src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold my-4">Welcome to the Quiz App</h1>
+      <p>
+        This is a simple audio recording quiz application. Click on "Quiz" to start the quiz and answer the questions
+        by recording your voice. You can view your statistics in the "Statistics" section.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
